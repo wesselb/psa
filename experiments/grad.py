a168041f31d4dea9b3bb39268a28d72dc09d964d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-22 12:38:07 +0100

    Adjust gradient estimation experiment

--- experiments/grad.py
+++ experiments/grad.py
@@ -14,13 +14,13 @@ B.default_dtype = jnp.float32
 
 # Setting of experiment:
 x = B.linspace(0, 10, 500)
-m = 1
-p = 3
+m = 2
+p = 4
 true_basis = Vars(jnp.float32).orthogonal(shape=(p, p))
 
 # Build a model for the data.
 prior = Measure()
-z_model = [GP(0.5 * EQ() + 0.5 * Delta(), measure=prior) for _ in range(m)]
+z_model = [GP(0.95 * EQ() + 0.05 * Delta(), measure=prior) for _ in range(m)]
 z_model += [GP(Delta(), measure=prior) for _ in range(p - m)]
 y_model = [sum([true_basis[j, i] * z_model[i] for i in range(p)], 0) for j in range(p)]
 
