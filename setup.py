1909bd088de0734a4013622d05da7fc6fc023a84
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-24 16:54:55 +0100

    Add varz to requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import find_packages, setup
 
-requirements = ["numpy>=1.16", "backends", "stheno", "jax", "jaxlib"]
+requirements = [
+    "numpy>=1.16",
+    "backends",
+    "varz>=0.5.3",
+    "stheno",
+    "wbml",
+    "jax",
+    "jaxlib",
+]
 
 setup(
     packages=find_packages(exclude=["docs"]),
