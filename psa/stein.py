6beb2e6c7bfafea31fde9e56d5a2ad2c27087c90
Eric Perim <eric.perim@invenialabs.co.uk>
2021-01-25 21:37:05 +0000

    Add bandwidth estimators

--- psa/stein.py
+++ psa/stein.py
@@ -1,19 +1,26 @@
 import jax
 import lab.jax as B
+from .util import select_bandwidth
 
 
 __all__ = ["stein", "stein_conditional"]
 
-
 def _k(x, h):
-    gram = B.exp(-0.5 * B.pw_dists2(x) / h ** 2)
+    gram = B.exp(-0.5 * B.pw_dists2(x / (h ** 2)))
 
     def dk(i):
         xi = x[:, i]
-        return (xi[:, None] - xi[None, :]) / h ** 2 * gram
+        return (xi[:, None] - xi[None, :]) / h[i] ** 2 * gram
 
     return gram, dk
 
+control_flow_cache = B.ControlFlowCache() # trying to use https://wesselb.github.io/2021/01/19/linear-models-with-stheno-and-jax.html
+# but failing
+
+@jax.jit
+def _k_jitted(x, h):
+    with control_flow_cache:
+        return _k(x, h)
 
 @jax.jit
 def stein(x, h, eta=1e-2):
@@ -27,7 +34,8 @@ def stein(x, h, eta=1e-2):
     Returns:
         matrix: Estimate of the gradients of `log p(x)` with respect to `x`.
     """
-    gram, dk = _k(x, h)
+    scale = select_bandwidth(x, None) # ignoring h in an ugly way
+    gram, dk = _k_jitted(x, scale)
     chol = B.cholesky(B.reg(gram, diag=eta))
     inds = range(B.shape(x)[1])  # Get gradient w.r.t. all dimensions.
     return -B.cholsolve(chol, B.stack(*[B.sum(dk(i), axis=1) for i in inds], axis=1))
@@ -47,8 +55,10 @@ def stein_conditional(x, y, h, eta=1e-2):
         tuple[matrix,matrix]: Estimates of the gradients of `log p(x|y)` with respect
             to `x` and `y`.
     """
-    grad_joint = stein(B.concat(x, y, axis=1), h=h, eta=eta)
+    scale = select_bandwidth(B.concat(x, y, axis=1), None) # ignoring h in an ugly way
+    grad_joint = stein(B.concat(x, y, axis=1), h=scale, eta=eta)
     grad_joint_x = grad_joint[:, : B.shape(x)[1]]
     grad_joint_y = grad_joint[:, B.shape(x)[1] :]
-    grad_marg_y = stein(y, h=h, eta=eta)
+    scale = select_bandwidth(y, None)
+    grad_marg_y = stein(y, h=scale, eta=eta)
     return grad_joint_x, grad_joint_y - grad_marg_y
--- psa/util.py
+++ psa/util.py
@@ -1,6 +1,8 @@
 import lab as B
+import jax.numpy as jnp
+import jax
 
-__all__ = ["cos_sim"]
+__all__ = ["cos_sim", "select_bandwidth"]
 
 
 def cos_sim(x, y):
@@ -16,3 +18,161 @@ def cos_sim(x, y):
     norm_x = B.sqrt(B.sum(x ** 2))
     norm_y = B.sqrt(B.sum(y ** 2))
     return B.sum(x * y) / norm_x / norm_y
+
+#### Bandwidth estimation
+# Adapted from https://www.statsmodels.org/stable/_modules/statsmodels/nonparametric/bandwidths.html
+
+@jax.jit
+def _select_sigma(x, percentile=25):
+    """
+    Returns the smaller of std(X, ddof=1).
+
+    References
+    ----------
+    Silverman (1986) p.47
+    """
+    normalize = 1.349
+    IQR = (jnp.percentile(x, 75) - jnp.percentile(x, 25)) / normalize
+    std_dev = jnp.std(x, axis=0, ddof=1)
+    return jnp.minimum(std_dev, IQR)
+
+
+## Univariate Rule of Thumb Bandwidths ##
+@jax.jit
+def bw_scott(x, kernel=None):
+    """
+    Scott's Rule of Thumb
+
+    Parameters
+    ----------
+    x : array_like
+        Array for which to get the bandwidth
+    kernel : CustomKernel object
+        Unused
+
+    Returns
+    -------
+    bw : float
+        The estimate of the bandwidth
+
+    Notes
+    -----
+    Returns 1.059 * A * n ** (-1/5.) where ::
+
+       A = min(std(x, ddof=1), IQR/1.349)
+       IQR = jnp.subtract.reduce(jnp.percentile(x, [75,25]))
+
+    References
+    ----------
+
+    Scott, D.W. (1992) Multivariate Density Estimation: Theory, Practice, and
+        Visualization.
+    """
+    A = _select_sigma(x)
+    n = len(x)
+    return 1.059 * A * n ** (-0.2)
+
+@jax.jit
+def bw_silverman(x, kernel=None):
+    """
+    Silverman's Rule of Thumb
+
+    Parameters
+    ----------
+    x : array_like
+        Array for which to get the bandwidth
+    kernel : CustomKernel object
+        Unused
+
+    Returns
+    -------
+    bw : float
+        The estimate of the bandwidth
+
+    Notes
+    -----
+    Returns .9 * A * n ** (-1/5.) where ::
+
+       A = min(std(x, ddof=1), IQR/1.349)
+       IQR = jnp.subtract.reduce(jnp.percentile(x, [75,25]))
+
+    References
+    ----------
+
+    Silverman, jnp.W. (1986) `Density Estimation.`
+    """
+    A = _select_sigma(x)
+    n = len(x)
+    return .9 * A * n ** (-0.2)
+
+
+@jax.jit
+def bw_normal_reference(x, kernel=None):
+    """
+    Plug-in bandwidth with kernel specific constant based on normal reference.
+
+    This bandwidth minimizes the mean integrated square error if the true
+    distribution is the normal. This choice is an appropriate bandwidth for
+    single peaked distributions that are similar to the normal distribution.
+
+    Parameters
+    ----------
+    x : array_like
+        Array for which to get the bandwidth
+    kernel : CustomKernel object
+        Used to calculate the constant for the plug-in bandwidth.
+        The default is a Gaussian kernel.
+
+    Returns
+    -------
+    bw : float
+        The estimate of the bandwidth
+
+    Notes
+    -----
+    Returns C * A * n ** (-1/5.) where ::
+
+       A = min(std(x, ddof=1), IQR/1.349)
+       IQR = jnp.subtract.reduce(jnp.percentile(x, [75,25]))
+       C = constant from Hansen (2009)
+
+    When using a Gaussian kernel this is equivalent to the 'scott' bandwidth up
+    to two decimal places. This is the accuracy to which the 'scott' constant is
+    specified.
+
+    References
+    ----------
+
+    Silverman, jnp.W. (1986) `Density Estimation.`
+    Hansen, jnp.E. (2009) `Lecture Notes on Nonparametrics.`
+    """
+    C = 1.0592238410488122
+    A = _select_sigma(x)
+    n = len(x)
+    return C * A * n ** (-0.2)
+
+@jax.jit
+def select_bandwidth(x, kernel):
+    """
+    Selects bandwidth for a selection rule bw
+
+    this is a wrapper around existing bandwidth selection rules
+
+    Parameters
+    ----------
+    x : array_like
+        Array for which to get the bandwidth
+    bw : str
+        name of bandwidth selection rule, currently supported are:
+        %s
+    kernel : not used yet
+
+    Returns
+    -------
+    bw : float
+        The estimate of the bandwidth
+    """
+    bandwidth = bw_normal_reference(x, kernel) # hardcoding it here because JAX is annoying
+    # there are two other options: bw_silverman and bw_scott, but passing this as an argument
+    # was making it break
+    return bandwidth
